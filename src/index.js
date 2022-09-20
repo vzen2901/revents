d95f0eb3dc4724c2f78a3073b7401af684ceed06
hanhvuong <vzen2901@gmail.com>
2022-09-20 19:55:38 +0700

    module.hot

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const render = root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+if(module.hot) {
+  module.hot.accept('./App', () => {
+    setTimeout(render);
+  })
+}
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
