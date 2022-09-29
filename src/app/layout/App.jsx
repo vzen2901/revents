1c27cbbdcb2466ba09b6870a28a27e646f5e7e0e
hanhvuong <vzen2901@gmail.com>
2022-09-29 18:41:43 +0700

    add event

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import React from 'react';
+import { Component, Fragment } from 'react';
+import EventDashboard from '../../component/event/eventDashboard/EventDashboard';
+import NavBar from '../../component/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <Fragment>
+        <NavBar />
+        <Container classname="main"> 
+          <EventDashboard />
+        </Container>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/event/eventList/EventListItem.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class EventListItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default EventListItem;
